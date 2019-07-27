67fe67517fb84ad99bace35e97b3b91dafe818e0
Jonjo McKay <jonjo@jonjomckay.com>
2019-07-27 19:01:55 +0100

    Improved LoadableButton

--- src/LoadableButton.tsx
+++ src/LoadableButton.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 
 interface LoadableButtonProps {
     onClick(...args: any[]): void
@@ -8,9 +9,14 @@ interface LoadableButtonProps {
 }
 
 const LoadableButton: React.FunctionComponent<LoadableButtonProps> = ({ children, isLoading, onClick }) => {
+    let spinner;
+    if (isLoading) {
+        spinner = <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" />
+    }
+
     return (
         <Button disabled={ isLoading } onClick={ onClick }>
-            { children }
+            { spinner || children }
         </Button>
     );
 };
